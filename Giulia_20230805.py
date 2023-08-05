fb45036bd4caf512c6ad07d7332c6f2e8a3f7833
Giulia Biasi <giulia.biasi18@imperial.ac.uk>
2023-08-05 19:03:51 +0100

    functions that need to be added to primers functions

--- Giulia_20230805.py
+++ Giulia_20230805.py
@@ -24,22 +24,93 @@ def concatenate_for_primer_mutation(primers_output_file, optimal_sgRNA_output_fi
     concat_primers.to_excel("outputFiles\output_concat_primers_and_sgRNA_prior_to_mutation.xlsx")
     return concat_primers
 
+
+def revComp(inputSeq):
+    """
+    This function takes an input sequence and returns the reverse complement.
+
+    Input: inputSeq in str format
+    Output: revComp in str format
+
+    """
+    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
+
+    revComp = ""
+    for base in inputSeq[::-1]:
+        revComp += complement[(base.upper())]
+
+    return revComp
+
+
+def primer_parser(primer_sequence, primer_type, strand_type):
+
+    primer_length = len(primer_sequence)
+    leftover_size = primer_length%3
+    splitat = primer_length - leftover_size
+
+    if strand_type == '-':
+
+        if primer_type == 'hal-r':
+            shortened_primer, leftover = primer_sequence[:splitat], primer_sequence[splitat:]
+        if primer_type == 'har-f':
+            primer_sequence_r = revComp (primer_sequence)
+            shortened_primer, leftover = primer_sequence_r[:leftover_size], primer_sequence_r[leftover_size:]
+
+    if strand_type == '+':
+        if primer_type == 'hal-r':
+            primer_sequence_r = revComp(primer_sequence)
+            shortened_primer, leftover = primer_sequence_r[:leftover_size], primer_sequence_r[leftover_size:]
+        if primer_type == 'har-f':
+            shortened_primer, leftover = primer_sequence[:splitat], primer_sequence[splitat:]
+
+    return shortened_primer, leftover
+
+
 def mutate_PAM_in_primers(concat_primers):
 
-    #for index, row in concat_primers.iterrows():
+    concat_primers['mutated_primers_?'] = 'mutation was not necessary'
 
-        #body of the function, or call out to fucntion that actually mutates
-        # if column says yes then pass to function
-        #df_updated = fct.mutate_PAM_in_HDR_plasmid(row['HAL-R'],row['HAR-F'],df)
-        #1. check what was mutated - add this info to the extra column
-        #add column for the Pam in concatenate_for_primer_mutation()
-        #fill in
+    for index, row in concat_primers.iterrows():
 
-        #row['HAR-F']=df_updated['HAR-F']
-        #row['HAL-R'] = df_updated['HAL-R']
+        if row['must_PAM_be_mutated_in_HDR_plasmid?'] != 'no':
+
+            #@TODO: must make sure that df is being passed in the right df format and that nothing is being left out
+            df = {
+                "start/stop": row['Gene_Region'],
+                'genome_start_codon_pos': row['genome_start_codon_pos'],
+                'genome_stop_codon_pos': row['genome_stop_codon_pos'],
+                'strand_type': row['strand_type'],
+                "sgRNA_list_positions": [row['sgRNA_start'], row['sgRNA_start']],
+                "sgRNA_list_values": row['sgRNA_seq']
+            }
+
+            hal_r_parsed, hal_r_leftover = primer_parser(row['HAL-R'],'hal-r',row['strand_type'])
+            har_f_parsed, har_f_leftover = primer_parser(row['HAR-F'],'har-f',row['strand_type'])
+
+            #@TODO - uncomment this - df_updated = mutate_PAM_in_HDR_plasmid(hal_r_parsed,har_f_parsed,df)
+
+            df_updated = {
+                "start/stop": row['Gene_Region'],
+                'genome_start_codon_pos': row['genome_start_codon_pos'],
+                'genome_stop_codon_pos': row['genome_stop_codon_pos'],
+                'strand_type': row['strand_type'],
+                "sgRNA_list_positions": [row['sgRNA_start'], row['sgRNA_start']],
+                "sgRNA_list_values": row['sgRNA_seq'],
+                "HAR-F": "ParsedAndMutated",
+                "HAL-R": "ParsedAndMutated",
+                "mutated?": "MutationPlaceholder"
+            }
+
+            #1. check what was mutated - add this info to the extra column
+            #add column for the Pam in concatenate_for_primer_mutation()
+            #fill in
+
+            #row['HAR-F']=df_updated['HAR-F']
+            #row['HAL-R'] = df_updated['HAL-R']
 
 
     #concat_primers.to_excel("outputFiles\output_concat.xlsx")
+    print(concat_primers)
     return concat_primers
 
 
@@ -47,5 +118,6 @@ optimal_sgRNA_output_file = "inputfiles\mockMaterials\optimal_sgRNAs_mock.xlsx"
 primers_outputfile_nomutation = "inputfiles\mockMaterials\mockTFsdfwithPrimers.xlsx"
 
 concatenated_primers_sgRNA = concatenate_for_primer_mutation(primers_outputfile_nomutation, optimal_sgRNA_output_file)
-print(concatenated_primers_sgRNA)
+mutated_primers = mutate_PAM_in_primers(concatenated_primers_sgRNA)
 
+#check if the PAM is
