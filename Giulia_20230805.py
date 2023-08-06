3e2f3c01ddd2a48e8930b0ba189a59b5dee459b2
Giulia Biasi <giulia.biasi18@imperial.ac.uk>
2023-08-06 18:36:41 +0100

    functions that need to be added to primers functions

--- Giulia_20230805.py
+++ Giulia_20230805.py
@@ -24,7 +24,6 @@ def concatenate_for_primer_mutation(primers_output_file, optimal_sgRNA_output_fi
     concat_primers.to_excel("outputFiles\output_concat_primers_and_sgRNA_prior_to_mutation.xlsx")
     return concat_primers
 
-
 def revComp(inputSeq):
     """
     This function takes an input sequence and returns the reverse complement.
@@ -41,7 +40,6 @@ def revComp(inputSeq):
 
     return revComp
 
-
 def primer_parser(primer_sequence, primer_type, strand_type):
 
     primer_length = len(primer_sequence)
@@ -54,21 +52,125 @@ def primer_parser(primer_sequence, primer_type, strand_type):
             shortened_primer, leftover = primer_sequence[:splitat], primer_sequence[splitat:]
         if primer_type == 'har-f':
             primer_sequence_r = revComp (primer_sequence)
-            shortened_primer, leftover = primer_sequence_r[:leftover_size], primer_sequence_r[leftover_size:]
+            leftover, shortened_primer = primer_sequence_r[:leftover_size], primer_sequence_r[leftover_size:]
 
     if strand_type == '+':
         if primer_type == 'hal-r':
             primer_sequence_r = revComp(primer_sequence)
-            shortened_primer, leftover = primer_sequence_r[:leftover_size], primer_sequence_r[leftover_size:]
+            leftover, shortened_primer = primer_sequence_r[:leftover_size], primer_sequence_r[leftover_size:]
         if primer_type == 'har-f':
             shortened_primer, leftover = primer_sequence[:splitat], primer_sequence[splitat:]
 
     return shortened_primer, leftover
 
+def primer_collator(mutated_shortened_primer, leftover, primer_type, strand_type):
+
+    if strand_type == '+':
+        if primer_type == 'hal-r':
+            updated_primer = leftover + mutated_shortened_primer
+        if primer_type == 'har-f':
+            updated_primer = mutated_shortened_primer + leftover
+    if strand_type == '-':
+        if primer_type == 'hal-r':
+            updated_primer = mutated_shortened_primer + leftover
+        if primer_type == 'har-f':
+            updated_primer = leftover + mutated_shortened_primer
+
+    return updated_primer
+
+def primer_collator_tester():
+
+    collated_string = primer_collator('MutatedPrimer ', 'Leftover ', 'hal-r','+')
+    print('hal-r,+:', collated_string)
+    collated_string = primer_collator('MutatedPrimer ', 'Leftover ', 'har-f', '+')
+    print('har-f,+:', collated_string)
+    collated_string = primer_collator('MutatedPrimer ', 'Leftover ', 'hal-r', '-')
+    print('hal-r,-:', collated_string)
+    collated_string = primer_collator('MutatedPrimer ', 'Leftover ', 'har-f', '-')
+    print('har-f,-:', collated_string)
+
+    return
+#primer_collator_tester()
+
+def primer_parser_tester():
+
+    A, B = primer_parser('AAAAAACC', 'hal-r','-')
+    print(A)
+    print(B)
+    A, B = primer_parser('AAAAAACC', 'har-f','-')
+    print(A)
+    print(B)
+    A, B = primer_parser('AAAAAACC', 'hal-r','+')
+    print(A)
+    print(B)
+    A, B = primer_parser('AAAAAACC', 'har-f','+')
+    print(A)
+    print(B)
+
+    return
+#primer_parser_tester()
+
+def PAM_sgRNA_presence_checker(gene_region, sgRNA_strand_type, sgRNA_end,
+                               sgRNA_start, HAL_R, HAR_F,
+                               strand_type, start_codon_position, stop_codon_position):
+
+    if gene_region == 'start_codon':
+        startstop_codon_position = start_codon_position
+    elif gene_region == 'stop_codon':
+        startstop_codon_position = stop_codon_position
+
+    if strand_type == '+':
+        primer_region_start = startstop_codon_position - len(HAL_R)
+        primer_region_stop = startstop_codon_position + 2 + len(HAR_F)
+    elif strand_type == '-':
+        primer_region_start = startstop_codon_position - 2 - len(HAL_R)
+        primer_region_stop = startstop_codon_position + len(HAR_F)
+
+    # check the PAM and recognition site
+    if sgRNA_strand_type == '+':
+        PAM_start = sgRNA_end - 2
+        PAM_stop = sgRNA_end
+        recognition_area_start = PAM_start - 6
+        recognition_area_stop = PAM_start - 1
+    elif sgRNA_strand_type == '-':
+        PAM_start = sgRNA_start - 2
+        PAM_stop = sgRNA_start
+        recognition_area_start = PAM_start + 6
+        recognition_area_stop = PAM_start + 1
+
+    if PAM_start > primer_region_start and PAM_stop < primer_region_stop:
+        PAM_in_primers = True
+    else:
+        PAM_in_primers = False
+
+    if recognition_area_start > primer_region_start and recognition_area_stop < primer_region_stop:
+        sgRNA_recognition_in_primers = True
+    else:
+        sgRNA_recognition_in_primers = False
+
+    return PAM_in_primers, sgRNA_recognition_in_primers
+
+def loop_checker_PAM_sgRNA(concat_primers):
+    concat_primers['is PAM in primer?'] = 'unknown yet'
+    concat_primers['is recognition site in primer?'] = 'unknown yet'
+
+    for index, row in concat_primers.iterrows():
+        PAM_in_primers, sgRNA_recognition_in_primers = PAM_sgRNA_presence_checker(row['Gene_Region'],
+                                                                                  row['sgRNA_strand'],
+                                                                                  row['sgRNA_end'],
+                                                                                  row['sgRNA_start'],
+                                                                                  row['HAL-R'], row['HAR-F'],
+                                                                                  row['strand_type'],
+                                                                                  row['genome_start_codon_pos'],
+                                                                                  row['genome_stop_codon_pos'])
+        concat_primers.at[index, 'is PAM in primer?'] = PAM_in_primers
+        concat_primers.at[index, 'is recognition site in primer?'] = sgRNA_recognition_in_primers
+
+    concat_primers.to_excel("outputFiles\primers_checked_for_PAM_and_sgRNA.xlsx")
+    return concat_primers
 
 def mutate_PAM_in_primers(concat_primers):
-
-    concat_primers['mutated_primers_?'] = 'mutation was not necessary'
+    concat_primers['were primers mutated?'] = 'mutation was not necessary'
 
     for index, row in concat_primers.iterrows():
 
@@ -109,8 +211,8 @@ def mutate_PAM_in_primers(concat_primers):
             #row['HAL-R'] = df_updated['HAL-R']
 
 
-    #concat_primers.to_excel("outputFiles\output_concat.xlsx")
-    print(concat_primers)
+    concat_primers.to_excel("outputFiles\output_concat.xlsx")
+    #print(concat_primers)
     return concat_primers
 
 
@@ -118,6 +220,5 @@ optimal_sgRNA_output_file = "inputfiles\mockMaterials\optimal_sgRNAs_mock.xlsx"
 primers_outputfile_nomutation = "inputfiles\mockMaterials\mockTFsdfwithPrimers.xlsx"
 
 concatenated_primers_sgRNA = concatenate_for_primer_mutation(primers_outputfile_nomutation, optimal_sgRNA_output_file)
-mutated_primers = mutate_PAM_in_primers(concatenated_primers_sgRNA)
-
-#check if the PAM is
+#mutated_primers = mutate_PAM_in_primers(concatenated_primers_sgRNA)
+PAM_or_sgRNA_in_primer = loop_checker_PAM_sgRNA(concatenated_primers_sgRNA)
