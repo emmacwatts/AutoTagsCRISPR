b2d5bc20269ce978e7b15b16296499f80c908d05
Marina <marina.luchner@gmail.com>
2024-01-26 11:06:56 +0000

    cleaning and final debugging

---
+++ oldFiles/tests_20230520.py
@@ -0,0 +1,13 @@
+#%%
+import pandas as pd
+primer_file = "inputfiles/Primers_output_GB.xlsx"
+
+queryTFsdf = pd.read_excel(primer_file)
+    
+display(queryTFsdf)
+
+#%%
+msg = "Hello World"
+print(msg)
+
+# %%
